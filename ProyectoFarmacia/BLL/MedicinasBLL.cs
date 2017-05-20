b64e8d846c8f3c42a51165db3e9a2c37f48a38ae
Roberti Amarante <yavier.ag@gmail.com>
2017-05-20 16:25:28 -0400

    Creado model Medicinas and BLL

---
+++ ProyectoFarmacia/BLL/MedicinasBLL.cs
@@ -0,0 +1,26 @@
+﻿using ProyectoFarmacia.DAL;
+using ProyectoFarmacia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFarmacia.BLL
+{
+    public class MedicinasBLL
+    {
+        public static bool Guardar(Medicinas medicina)
+        {
+            bool retorno = false;
+            using (var conexion = new WebFarmaciaDb())
+            {
+                conexion.Medicina.Add(medicina);
+
+                conexion.SaveChanges();
+
+                retorno = true;
+            }
+            return retorno;
+        }
+    }
+}
