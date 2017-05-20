861ad008f0e5032e59038837d205279c10ac6e01
seatiel ygnacio rosario <seatiel_2010@hotmail.com>
2017-05-20 16:00:17 -0400

    Clientes by Seatiel

---
+++ ProyectoFarmacia/BLL/ClientesBLL.cs
@@ -0,0 +1,32 @@
+﻿using ProyectoFarmacia.DAL;
+using ProyectoFarmacia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFarmacia.BLL
+{
+    public class ClientesBLL
+    {
+        public static bool Guardar(Clientes cliente)
+        {
+            bool retorno = false;
+            using (var db = new WebFarmaciaDb())
+            {
+                try
+                {
+                    db.Cliente.Add(cliente);
+                    db.SaveChanges();
+                    retorno = true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return retorno;
+        }
+    }
+}
---
+++ ProyectoFarmacia/DAL/WebFarmaciaDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFarmacia.DAL
+{
+    public class WebFarmaciaDb : DbContext 
+    {
+        public WebFarmaciaDb() : base("ConStr")
+        {
+
+        }
+        public DbSet<Models.Clientes> Cliente { get; set; }
+    }
+}
